fc5780a62f6af1b729bfde47d5b27ee0c07dd8fc
thabadcode <tchekgames@gmail.com>
2026-03-18 09:05:40 -0300

    feat: Cria o main-script.js e faz link para o HTML, implementa a limpeza dos campos ao clicar em 'Limpar'.

---
+++ scripts/main-script.js
@@ -0,0 +1,34 @@
+const btnClear = document.getElementById("btn-clear");
+const btnPdf = document.getElementById("btn-creat-pdf");
+
+const dateInput = document.getElementById("date");
+const clientInput = document.getElementById("client");
+const representativeInput = document.getElementById("representative");
+const paymentSelect = document.getElementById("payment");
+const observationsInput = document.getElementById("observations");
+
+const productInput = document.getElementById("product");
+const portageSelect = document.getElementById("portage");
+const capCheckBox = document.getElementById("cap");
+const contentCapInput = document.getElementById("content-cap");
+const unitSelect = document.getElementById("unit");
+const quantityInput = document.getElementById("quantity");
+const valuePriceInput = document.getElementById("value-price");
+
+btnClear.addEventListener("click", function() {
+    dateInput.value = "";
+    clientInput.value = "";
+    representativeInput.value = "";
+    paymentSelect.value = "";
+    observationsInput.value = "";
+
+    productInput.value = "";
+    portageSelect.value = "";
+    capCheckBox.checked = false;
+    contentCapInput.value = "";
+    unitSelect.value = "";
+    quantityInput.value = "";
+    valuePriceInput.value = "";
+
+    // Implementar lógica para limpar lista de produtos
+})

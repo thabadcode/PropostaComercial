1809dc0a872616c7cd952ac2a36d6644a9736ceb
thabadcode <tchekgames@gmail.com>
2026-03-19 10:44:15 -0300

    refactor: Remove o comentário sobre dúvida de sintaxe pois foi sanada.

--- scripts/product-model.js
+++ scripts/product-model.js
@@ -6,4 +6,4 @@ export class Product {
         this.unityPrice = unityPrice;
         this.subtotalPrice = subtotalPrice;
     }
-} // Tem uma forma mais enxuta de isso aqui ser feito? Tô sendo acusado de unused variável
+}
